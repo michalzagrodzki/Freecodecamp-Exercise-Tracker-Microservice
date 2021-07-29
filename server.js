564f676e1d13c6b4f73663db0ebb7b1a8f215c9a
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-29 12:21:07 +0200

    Outline boilerplate for exercise

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const bodyParser = require("body-parser");
+
+// Basic Configuration
+const { DEFAULT_PORT } = require("./utils/constants");
+
+const user_controller = require("./controllers/user");
+const exercise_controller = require("./controllers/exercise");
+const log_controller = require("./controllers/log");
+
+app.use(cors());
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const listener = app.listen(DEFAULT_PORT || process.env.PORT, function () {
+  console.log(`Listening on port ${listener.address().port}`);
+});
