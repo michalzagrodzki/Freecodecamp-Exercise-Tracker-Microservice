9c0adb3a6de039e0d74175f88e2c5c5a74a6226e
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-29 15:25:05 +0200

    Add endpoints

--- server.js
+++ server.js
@@ -17,9 +17,21 @@ app.use(cors());
 app.use("/public", express.static(`${process.cwd()}/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
+
+// info page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// user endpoints
+app.get("/api/users", user_controller.list);
+app.post("/api/users", user_controller.post);
+
+// exercises endpoints
+app.post("/api/users/:_id/exercises", exercise_controller.post);
+
+// logs endpoints
+app.get("/api/users/:_id/logs", log_controller.get);
 
 const listener = app.listen(DEFAULT_PORT || process.env.PORT, function () {
   console.log(`Listening on port ${listener.address().port}`);
