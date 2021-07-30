9c211da5adc368b4d92537de10265cbc574f32c8
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-30 16:11:57 +0200

    Correct method for validation of special characters

--- domains/user/user.middleware.js
+++ domains/user/user.middleware.js
@@ -10,7 +10,7 @@ const is = {
 const validate = {
   username: (payload) => {
     noSpecialCharacters = (payload) => {
-      regex = new RegExp(/^[a-zA-Z0-9!@,._-]+$/g);
+      regex = new RegExp(/^[a-zA-Z0-9!@,._\s-]+$/g);
       return regex.test(payload);
     };
     if (noSpecialCharacters(payload)) return;
